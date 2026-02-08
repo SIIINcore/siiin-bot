50e1c0323635d3808d141dfc5a24c00cc584655c
SIIINcore <ayzenwald@gmail.com>
2026-02-08 04:46:23 +0100

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+        if (!interaction.isButton()) return;
+        
+        console.log(`🖱️ Interaction: ${interaction.customId} par ${interaction.user.tag}`);
+        
+        // Logique des tickets sera ajoutée plus tard
+    }
+};
