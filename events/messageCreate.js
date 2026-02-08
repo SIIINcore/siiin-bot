b8c46bc39e7854328b710f043327012f433bdbd0
SIIINcore <ayzenwald@gmail.com>
2026-02-08 04:45:02 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,13 @@
+const { STAFF_IDS } = require('../config/constants');
 
+module.exports = {
+    name: 'messageCreate',
+    async execute(message, client) {
+        if (message.author.bot || !message.guild) return;
+        
+        // Ici on mettra l'automod et les tickets plus tard
+        // Pour l'instant, on garde la logique dans index.js
+        
+        console.log(`📝 Message de ${message.author.tag}: ${message.content.substring(0, 50)}...`);
+    }
+};
