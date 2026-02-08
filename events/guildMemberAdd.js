aaa1852a5203453b68411da317a2c84140b55cee
SIIINcore <ayzenwald@gmail.com>
2026-02-08 04:46:01 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,11 @@
+const { CHANNEL_WELCOME, BOT_VERSION } = require('../config/constants');
 
+module.exports = {
+    name: 'guildMemberAdd',
+    async execute(member, client) {
+        console.log(`👋 ${member.user.tag} a rejoint le serveur!`);
+        
+        // Logique du welcome sera ajoutée plus tard
+        // Pour l'instant juste un log
+    }
+};
