f4f90d60867d20cf2e9f235b8d58e918eed0a1aa
SIIINcore <ayzenwald@gmail.com>
2026-02-08 08:29:24 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -37,13 +37,13 @@ Enjoy your stay and check out the links below!
 
 ▫▫▫▫ **C H E C K** ▫▫▫▫
 # ─── ✦ INFORMATION ✦ ───
-<:cryengine:1033530974107091035> [Information](https://discord.com/channels/1033462383798140981/1033506664810287134/1440058017545584871)
+<:cryengine:1033530974107091035> [Information](https://discord.com/channels/1033462383798140978/1033506664810287134/1440058017545584871)
 <:cryengine:1033530974107091035> [Rules](https://discord.com/channels/1033462383798140978/1177257234787471422/1468570201095274552)
-<:cryengine:1033530974107091035> [Announcements](https://discord.com/channels/1033462383798140981/1237650687249092670)
-<:cryengine:1033530974107091035> [Search](https://discord.com/channels/1033462383798140981/1376910830490095798/1376912016517763094)
-<:cryengine:1033530974107091035> [Games List](https://discord.com/channels/1033462383798140981/1376904260842819685/1409551551818760204)
-<:cryengine:1033530974107091035> [Crysis and Crysis Warhead](https://discord.com/channels/1033462383798140981/1371242516556415098/1371242762417995776)
-<:cryengine:1033530974107091035> [Crysis Remastered](https://discord.com/channels/1033462383798140981/1372560937000763484/1372565847591092385)
+<:cryengine:1033530974107091035> <#1237650687249092670>
+<:cryengine:1033530974107091035> [Search](https://discord.com/channels/1033462383798140978/1376910830490095798/1376912016517763094)
+<:cryengine:1033530974107091035> [Games List](https://discord.com/channels/1033462383798140978/1376904260842819685/1409551551818760204)
+<:cryengine:1033530974107091035> [Crysis and Crysis Warhead](https://discord.com/channels/1033462383798140978/1371242516556415098/1371242762417995776)
+<:cryengine:1033530974107091035> [Crysis Remastered](https://discord.com/channels/1033462383798140978/1372560937000763484/1372565847591092385)
 
 # ─── ✦ PLATFORMS ✦ ───
 **STEAM | GOG | EA | UBISOFT | CD-ROM**
@@ -52,7 +52,7 @@ Enjoy your stay and check out the links below!
 **Check 1st the [Support rules](https://discord.com/channels/1033462383798140978/1379581746466783385/1379582509062426754) ► Then use ► The <#1468090646442279206> system.**
 
 # ─── ✦ DONATIONS ✦ ───
-To support us, please feel free to donate a bit. [Just Here](https://discord.com/channels/1033462383798140981/1178517213444046948/1351344918546874461)
+To support us, please feel free to donate a bit. [Just Here](https://discord.com/channels/1033462383798140978/1178517213444046948/1469955649227198536)
 [Donations Direct link](https://www.paypal.com/paypalme/LunaSiiin?)
 `;
 
