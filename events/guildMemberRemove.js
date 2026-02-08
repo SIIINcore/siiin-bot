cc8c9e0e666cba55b2858db158fbca7114c72d4e
SIIINcore <ayzenwald@gmail.com>
2026-02-08 05:18:01 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,1 +1,19 @@
+const { STATS_CHANNEL_ID } = require('../config/constants');
 
+module.exports = {
+    name: 'guildMemberRemove',
+    async execute(member, client) {
+        console.log(`👋 ${member.user.tag} left the server.`);
+        
+        // Update stats
+        setTimeout(async () => {
+            try {
+                const { updateStatsEmbed } = require('./ready');
+                const { postedGames, postedPromos, postedFreeToPlay } = require('../functions/contentUpdater');
+                await updateStatsEmbed(member.guild, client, postedGames, postedPromos, postedFreeToPlay);
+            } catch (err) {
+                console.error('[Stats Update] Error:', err.message);
+            }
+        }, 5000);
+    }
+};
