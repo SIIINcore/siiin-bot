97bf371b2a77e41f0b4838066ca51a9e57c4f30d
SIIINcore <ayzenwald@gmail.com>
2026-02-08 04:19:30 +0100

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,1 +1,37 @@
-https://github.com/SIIINcore/siiin-bot/tree/main/config/constants.js
+// config/constants.js
+module.exports = {
+    // ========================
+    // CHANNELS ID
+    // ========================
+    CHANNEL_FREEGAMES: '1469855556356542649',
+    CHANNEL_PROMOS: '1469855518695624725',
+    CHANNEL_FREETOPLAY: '1468126872297672928',
+    CHANNEL_WELCOME: '1033462383798140981',
+    STATS_CHANNEL_ID: '1465938751208558643',
+    SUPPORT_CHANNEL_ID: '1468090646442279206',
+    TICKET_CATEGORY_ID: '1237716160842305566',
+    LOG_CHANNEL_ID: '1354801906161025236',
+    CHAT_CHANNEL_ID: '1189391329097166989',
+    DONATION_CHANNEL_ID: '1178517213444046948',
+    BAN_LOG_CHANNEL_ID: '1417568141428396063',
+
+    // ========================
+    // APP ID | SIIIN CORE
+    // ========================
+    BOT_ID: '1465878128219128005',
+
+    // ========================
+    // STAFF ID
+    // ========================
+    STAFF_IDS: ['847798063821225985', '400331452245344268'],
+
+    // ========================
+    // TICKETS RULES
+    // ========================
+    ALLOWED_FILE_EXTENSIONS: ['.png', '.jpg', '.jpeg', '.gif', '.mp4', '.mov'],
+
+    // ========================
+    // BOT VERSION
+    // ========================
+    BOT_VERSION: '3.0.0.1'
+};
