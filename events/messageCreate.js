0fd24c77b0620e5bef5abf806039561240a32637
SIIINcore <ayzenwald@gmail.com>
2026-03-29 16:31:55 +0200

    Integrate translation offering in message handler
    Add translation helper functionality to message handling

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,18 @@
 const { handleAutomod } = require('../functions/automod');
 const { handleTicketMessageFilter } = require('../functions/tickets');
 const { CHAT_CHANNEL_ID, STAFF_IDS } = require('../config/constants');
+const { shouldOfferTranslation, attachTranslationHelper } = require('../functions/translation');
 
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
-        if (message.author.bot || !message.guild) return;
+        if (!message.guild) return;
+
+        if (shouldOfferTranslation(message, client)) {
+            await attachTranslationHelper(message);
+        }
+
+        if (message.author.bot) return;
         
         // Automod
         const isViolation = await handleAutomod(message);
