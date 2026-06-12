8075814fe00d741fcedd890ffc9eb79dc1813c75
SIIINcore <ayzenwald@gmail.com>
2026-06-12 02:20:12 +0200

    fix: Make messageCreate.js completely safe (remove all translation references)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,7 @@
 const { handleAutomod } = require('../functions/automod');
 const { handleTicketMessageFilter } = require('../functions/tickets');
 const { CHAT_CHANNEL_ID, STAFF_IDS } = require('../config/constants');
+
 const { updateChatReminder } = require('../functions/contentUpdater');
 
 module.exports = {
@@ -9,19 +10,22 @@ module.exports = {
         if (!message.guild) return;
         if (message.author.bot) return;
 
+        // Automod
         const isViolation = await handleAutomod(message);
         if (isViolation) return;
 
+        // Ticket channel filter
         if (message.channel.name.startsWith('ticket-')) {
             await handleTicketMessageFilter(message);
         }
 
+        // Chat reminder
         if (message.channel.id === CHAT_CHANNEL_ID && !STAFF_IDS.includes(message.author.id)) {
             setTimeout(async () => {
                 try {
                     await updateChatReminder(message.channel);
                 } catch (err) {
-                    console.error('[ChatReminder Update] Error:', err.message);
+                    console.error('[ChatReminder] Error:', err.message);
                 }
             }, 1000);
         }
