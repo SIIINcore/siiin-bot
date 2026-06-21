daa54f0d340270e5da42914e6220c1684a4abdce
SIIINcore <ayzenwald@gmail.com>
2026-06-21 19:04:35 +0200

    Refactor constants in config/constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
-// config/constants.js - Cleaned version
 module.exports = {
+    // Channels
     CHANNEL_FREEGAMES: '1469855556356542649',
     CHANNEL_PROMOS: '1469855518695624725',
     CHANNEL_FREETOPLAY: '1468126872297672928',
@@ -14,13 +14,42 @@ module.exports = {
     DONATION_CHANNEL_ID: '1178517213444046948',
     BAN_LOG_CHANNEL_ID: '1417568141428396063',
     TICKET_TRANSCRIPT_CHANNEL_ID: '1487595893803978772',
-    BOT_ID: '1465878128219128005',
+
+    // Rôles & Staff
+    MEMBER_ROLE_ID: '1033463588934918164',
     STAFF_IDS: ['847798063821225985', '400331452245344268'],
+
+    // Bot
+    BOT_ID: '1465878128219128005',
+    BOT_VERSION: require('../package.json').version,
+
+    // Automod
     AUTOMOD: {
-        BAD_WORDS: [ /\bn+i+g+g+[ae3r]*\b/i, /\bf+a+g+(?:g+o+t+)?\b/i, /\bk+y+s+\b/i, /\bretard(?:ed)?\b/i, /\bslut\b/i, /\bwhore\b/i ],
-        DANGEROUS_LINKS: [ /discord\.(?:gift|gg)\/[^\s]+/i, /steamcommunity\.[^\s]*\/tradeoffer/i, /free[-_ ]?nitro/i, /grabify/i, /iplogger/i, /bit\.ly\//i, /tinyurl\.com\//i, /t\.me\//i ],
-        ALLOWED_DOMAINS: ['youtube.com', 'youtu.be', 'discord.com', 'discord.gg', 'store.steampowered.com', 'steamcommunity.com', 'gog.com', 'epicgames.com', 'cheapshark.com', 'appbrain.com', 'apps.apple.com', 'rss.marketingtools.apple.com']
+        BAD_WORDS: [
+            /\bn+i+g+g+[ae3r]*\b/i,
+            /\bf+a+g+(?:g+o+t+)?\b/i,
+            /\bk+y+s+\b/i,
+            /\bretard(?:ed)?\b/i,
+            /\bslut\b/i,
+            /\bwhore\b/i
+        ],
+        DANGEROUS_LINKS: [
+            /discord\.(?:gift|gg)\/[^\s]+/i,
+            /steamcommunity\.[^\s]*\/tradeoffer/i,
+            /free[-_ ]?nitro/i,
+            /grabify/i,
+            /iplogger/i,
+            /bit\.ly\//i,
+            /tinyurl\.com\//i,
+            /t\.me\//i
+        ],
+        ALLOWED_DOMAINS: [
+            'youtube.com', 'youtu.be', 'discord.com', 'discord.gg',
+            'store.steampowered.com', 'steamcommunity.com', 'gog.com',
+            'epicgames.com', 'cheapshark.com', 'appbrain.com',
+            'apps.apple.com', 'rss.marketingtools.apple.com'
+        ]
     },
-    ALLOWED_FILE_EXTENSIONS: ['.png', '.jpg', '.jpeg', '.gif', '.mp4', '.mov'],
-    BOT_VERSION: process.env.RAILWAY_GIT_COMMIT_SHA ? `git-${process.env.RAILWAY_GIT_COMMIT_SHA.slice(0, 7)}` : require('../package.json').version
+
+    ALLOWED_FILE_EXTENSIONS: ['.png', '.jpg', '.jpeg', '.gif', '.mp4', '.mov']
 };
